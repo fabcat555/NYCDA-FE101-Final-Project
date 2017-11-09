155428ea8c0dcc259538960ce82601b9b4c566b6
fabcat555 <fabio_555@hotmail.it>
2017-11-08 21:28:39 -0500

    Added Google Maps in footer

--- js/main.js
+++ js/main.js
@@ -30,6 +30,260 @@ const sia = {
       $('#noresults-msg').show();
     });
   },
+
+  initMap: function() {
+    $('#map-container').fadeToggle();
+    window.scrollTo(0, document.body.scrollHeight);
+  
+    const mapContainer = document.getElementById('map');
+    const mapCenter = {
+      lat: 40.707910,
+      lng: -74.006483
+    };
+    const styles = [
+      {
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#ebe3cd"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#523735"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          {
+            "color": "#f5f1e6"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+          {
+            "color": "#c9b2a6"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative.land_parcel",
+        "elementType": "geometry.stroke",
+        "stylers": [
+          {
+            "color": "#dcd2be"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative.land_parcel",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#ae9e90"
+          }
+        ]
+      },
+      {
+        "featureType": "landscape.natural",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#dfd2ae"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#dfd2ae"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#93817c"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "color": "#a5b076"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#447530"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#f5f1e6"
+          }
+        ]
+      },
+      {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#fdfcf8"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#f8c967"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+          {
+            "color": "#e9bc62"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway.controlled_access",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#e98d58"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway.controlled_access",
+        "elementType": "geometry.stroke",
+        "stylers": [
+          {
+            "color": "#db8555"
+          }
+        ]
+      },
+      {
+        "featureType": "road.local",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#806b63"
+          }
+        ]
+      },
+      {
+        "featureType": "transit.line",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#dfd2ae"
+          }
+        ]
+      },
+      {
+        "featureType": "transit.line",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#8f7d77"
+          }
+        ]
+      },
+      {
+        "featureType": "transit.line",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          {
+            "color": "#ebe3cd"
+          }
+        ]
+      },
+      {
+        "featureType": "transit.station",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#dfd2ae"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "color": "#b9d3c2"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#92998d"
+          }
+        ]
+      }
+    ];
+    const mapOptions = {
+      zoom: 16,
+      center: mapCenter,
+      styles: styles
+    };
+    
+    const contentString = '<div id="map-content">'+
+    '<h1 id="firstHeading" class="firstHeading">NYCDA</h1>'+
+    '<div id="bodyContent">'+
+    '<p>90 John Street</p>' +
+    '<p>New York, NY, 10038 USA</p>'
+    '</div>'+
+    '</div>';
+    
+    const infowindow = new google.maps.InfoWindow({
+      content: contentString
+    });
+    
+    const map = new google.maps.Map(mapContainer, mapOptions);
+    const marker = new google.maps.Marker({
+      position: mapCenter,
+      map: map,
+    });
+    marker.addListener('click', function() {
+      infowindow.open(map, marker);
+    });
+  
+    infowindow.open(map, marker);
+  },
   
   /* Triggers modal on team gallery's items */
   openHireModal: function() {
@@ -93,7 +347,8 @@ const sia = {
     });
     const removeButton = $('<a>', {
       class: 'remove-btn',
-      'data-target': superhero.attr('id')
+      'data-target': superhero.attr('id'),
+      text: '✖'
     });
     
     superheroDiv.hide().append(superheroImg).append(removeButton).fadeIn().css('display', 'inline');
@@ -227,4 +482,9 @@ $(document).ready(function(event) {
   
   /* Handles click outside of navbar on mobile */
   $(document).on('touchstart', sia.touchEvent);
+
+  /* Sets callback on address click (shows Google Maps) */
+  $('address').on('click', function() {
+    sia.initMap();
+  });
 });
