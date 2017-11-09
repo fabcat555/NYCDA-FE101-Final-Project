715b540dc6f4aeb6407678e2b6c75317f6e144e8
fabcat555 <fabio_555@hotmail.it>
2017-11-08 22:24:46 -0500

    Click outside navbar

--- js/main.js
+++ js/main.js
@@ -454,6 +454,7 @@ const sia = {
   
   /* Handles touch event on mobile (collapses navbar if expanded) */
   touchEvent: function() {
+    alert('ciao');
     const isNavbarCollapsed = $('.navbar-toggle').attr('aria-expanded');
     if (isNavbarCollapsed) {
       $('#nav-menu').collapse('hide');
